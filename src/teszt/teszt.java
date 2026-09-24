4fc12bc5d5383e50f7b5bee8f3fe34b193e14503
Frnc36 <magoriferenc19@gmail.com>
2026-09-24 15:43:13 +0200

    tesztek

--- src/teszt/teszt.java
+++ src/teszt/teszt.java
@@ -1,7 +1,76 @@
-
 package teszt;
 
+import app.Pakli;
+import app.Program;
 
 public class teszt {
+
     
-}
+    
+    
+    
+    
+    
+    static Pakli pakli = new Pakli();
+    static Program p = new Program();
+    
+    public static void main(String[] args) {
+
+        
+        
+        
+        
+        
+        
+        
+        tesztEgyEsHaromKozott();
+        tesztTombHossza();
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private static void tesztTombHossza() {
+        int vartHossz = 21;
+        int tombHossz = pakli.getLapok().length;
+        assert vartHossz == tombHossz : "Nem 21";
+        System.out.println("ok");
+    }
+
+    private static void tesztEgyEsHaromKozott() {
+        int min = 1;
+        int max = 3;
+        int oszlop = p.melyik();
+        assert min <=  oszlop : "nope";
+        assert max >=  oszlop : "nope";
+        System.out.println("oki");
+       
+    }
+
+}//class
