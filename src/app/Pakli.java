da16910e2ccf9ed5c865e1b878155189648ce853
molnarbea <molnarbeatrix11@gmail.com>
2026-09-24 12:58:28 +0200

    kezdetleges Pakli

--- src/app/Pakli.java
+++ src/app/Pakli.java
@@ -2,4 +2,24 @@ package app;
 
 public class Pakli {
 
+    private Lap[] lapok = new Lap[21];
+
+    public Pakli() {
+        feltolt();
+    }
+
+    public void feltolt() {
+            for (int i = 0; i < 21;) {
+                for (int j = 0; j < 4; j++) {
+                    for (int k = 0; k < 6; k++) {
+                        if(i<21){
+                            i++;
+                            lapok[i]=Lap(j,k);
+                        }
+                    }
+                }
+            }
+    }
+    
+    
 }
