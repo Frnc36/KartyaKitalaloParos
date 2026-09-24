07c74ae799274c28138df4148f2f711d1c9e2407
Frnc36 <magoriferenc19@gmail.com>
2026-09-24 13:04:29 +0200

    Lap osztaly

--- src/app/Lap.java
+++ src/app/Lap.java
@@ -2,4 +2,24 @@ package app;
 
 public class Lap {
 
-}
+    private String ertek;
+    private String szin;
+
+    public Lap(String ertek, String szin) {
+        this.ertek = ertek;
+        this.szin = szin;
+    }
+
+    public String getErtek() {
+        return ertek;
+    }
+
+    public String getSzin() {
+        return szin;
+    }
+
+    public String megjelenit() {
+        return szin + "_" + ertek;
+    }
+
+}//class
