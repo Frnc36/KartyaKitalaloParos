4b167375d3914aaead0dd4553a1ffe9e6332a88e
Molnár Beatrix Katalin <molnar.beatrix@akk.lan>
2026-09-24 13:34:44 +0200

    Feltolt javitva

--- src/app/Pakli.java
+++ src/app/Pakli.java
@@ -9,19 +9,30 @@ public class Pakli {
     }
 
     public void feltolt() {
-        String[] szin = {"P","T","Z","M"};
-        String[] ertek = {"Ász","Kir","Fel","X","IX","VIII"};
-            for (int i = 0; i < 21;) {
-                for (int j = 0; j < 4; j++) {
-                    for (int k = 0; k < 6; k++) {
-                        if(i<21){
+        String[] szinek = {"P","T","Z","M"};
+        String[] ertekek = {"Ász","Kir","Fel","X","IX","VIII"};
+        
+        int i = 0;
+        for (String szin : szinek) {
+            for (String ertek : ertekek) {
+                Lap lap = new Lap(szin,ertek);
+                    if(i<21){
+                            lapok[i]=lap;
                             i++;
-                            lapok[i]=new Lap(szin[j],ertek[k]);
                         }
                     }
                 }
-            }
+            
     }
+
+    public Lap[] getLapok() {
+        Lap[] ujLapok = lapok;
+        return ujLapok;
+    }
+    
+    
+    
+    
     
     
 }
