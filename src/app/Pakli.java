2e172d8b18a57d08037e9aa87f52517f5eebbd38
Molnár Beatrix Katalin <molnar.beatrix@akk.lan>
2026-09-24 13:13:40 +0200

    Feltolt metodus megirva

--- src/app/Pakli.java
+++ src/app/Pakli.java
@@ -9,12 +9,14 @@ public class Pakli {
     }
 
     public void feltolt() {
+        String[] szin = {"P","T","Z","M"};
+        String[] ertek = {"Ász","Kir","Fel","X","IX","VIII"};
             for (int i = 0; i < 21;) {
                 for (int j = 0; j < 4; j++) {
                     for (int k = 0; k < 6; k++) {
                         if(i<21){
                             i++;
-                            lapok[i]=Lap(j,k);
+                            lapok[i]=new Lap(szin[j],ertek[k]);
                         }
                     }
                 }
